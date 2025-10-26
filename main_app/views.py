f401d71d4656822868b5d323fecd60806f6c68c5
taif <eng.taifabdullah@gmail.com>
2025-10-26 21:20:43 +0300

    Adding in view Certificates CRUD (( Read And Create ))

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework import status
+from .models import Certificate
+from .serializers import CertificateSerializer
 # ______________________________________________________________________________________________________________
 
 class Home(APIView):
@@ -15,4 +18,26 @@ class Home(APIView):
             "message": "You just posted to LifeStack",
             "data": data
         })
+# ______________________________________________________________________________________________________________
+
+
+# Certificates CRUD (( Read And Create ))
+
+class CertificatesIndex(APIView):
+
+    def get(self, request):
+        queryset = Certificate.objects.all()
+        serializer = CertificateSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = CertificateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 # ______________________________________________________________________________________________________________
