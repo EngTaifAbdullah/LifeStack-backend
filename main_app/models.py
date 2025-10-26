0b3ed37acb23ba6efee79733f439235da08d578d
taif <eng.taifabdullah@gmail.com>
2025-10-26 19:30:29 +0300

    Add category model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+# ______________________________________________________________________________________________________________
 
 # Create your models here.
+
+# add catogory like: Task,Cource,Exam
+
+class Category(models.Model):
+    category_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_type
