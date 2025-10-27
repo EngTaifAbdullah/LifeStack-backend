a302f96f9f867ff8488b774096a61551e3b69453
taif <eng.taifabdullah@gmail.com>
2025-10-27 19:17:35 +0300

    Make it the URL for SECOND model

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from .views import Home, CertificatesIndex, CertificateDetail
+from .views import Home, CertificatesIndex, CertificateDetail, CoursesIndex, CourseDetail
+
+# __________________________________________________________________________________________________________________________
+
 
 urlpatterns = [
+
     path('', Home.as_view(), name='home'),
 
-  
+
     path('api/certificates/', CertificatesIndex.as_view(), name='cert_index'),
     path('api/certificates/<int:cert_id>/', CertificateDetail.as_view(), name='cert_detail'),
 
+   
+    path('api/courses/', CoursesIndex.as_view(), name='course_index'),
+    path('api/courses/<int:course_id>/', CourseDetail.as_view(), name='course_detail'),
 
 ]
