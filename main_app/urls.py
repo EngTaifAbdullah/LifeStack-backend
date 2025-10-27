ed9b88d04c5f2149f9455283c02f5bd6fad348f5
taif <eng.taifabdullah@gmail.com>
2025-10-27 19:20:05 +0300

    Make it the URL for THIRD model

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, CertificatesIndex, CertificateDetail, CoursesIndex, CourseDetail
+from .views import Home, CertificatesIndex, CertificateDetail, CoursesIndex, CourseDetail, PersonalDocsIndex, PersonalDocDetail
 
 # __________________________________________________________________________________________________________________________
 
@@ -11,9 +11,13 @@ urlpatterns = [
 
     path('api/certificates/', CertificatesIndex.as_view(), name='cert_index'),
     path('api/certificates/<int:cert_id>/', CertificateDetail.as_view(), name='cert_detail'),
-
    
+
     path('api/courses/', CoursesIndex.as_view(), name='course_index'),
     path('api/courses/<int:course_id>/', CourseDetail.as_view(), name='course_detail'),
+
+
+    path('api/personal/', PersonalDocsIndex.as_view(), name='personal_index'),
+    path('api/personal/<int:doc_id>/', PersonalDocDetail.as_view(), name='personal_detail'),
 
 ]
