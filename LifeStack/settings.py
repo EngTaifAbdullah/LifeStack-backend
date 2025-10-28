e69ddbb2b52f4b6e90eed78069fcb7523ef54f6a
taif <eng.taifabdullah@gmail.com>
2025-10-28 12:55:28 +0300

    Add corsheaders

--- LifeStack/settings.py
+++ LifeStack/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app',
+    'corsheaders'
+    
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,7 +63,7 @@ CORS_ALLOWED_ORIGINS = [
 
 ROOT_URLCONF = 'LifeStack.urls'
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
