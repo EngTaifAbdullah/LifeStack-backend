cecf76f7fc94bbef25505c955b57b542e048e9dd
taif <eng.taifabdullah@gmail.com>
2025-10-28 12:01:03 +0300

    Iupdated the URLs paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,15 +9,15 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
 
 
-    path('api/certificates/', CertificatesIndex.as_view(), name='cert_index'),
-    path('api/certificates/<int:cert_id>/', CertificateDetail.as_view(), name='cert_detail'),
-   
-
-    path('api/courses/', CoursesIndex.as_view(), name='course_index'),
-    path('api/courses/<int:course_id>/', CourseDetail.as_view(), name='course_detail'),
+    path('certificates/', CertificatesIndex.as_view(), name='cert_index'),
+    path('certificates/<int:cert_id>/', CertificateDetail.as_view(), name='cert_detail'),
 
 
-    path('api/personal/', PersonalDocsIndex.as_view(), name='personal_index'),
-    path('api/personal/<int:doc_id>/', PersonalDocDetail.as_view(), name='personal_detail'),
+    path('courses/', CoursesIndex.as_view(), name='course_index'),
+    path('courses/<int:course_id>/', CourseDetail.as_view(), name='course_detail'),
+
+
+    path('personal/', PersonalDocsIndex.as_view(), name='personal_index'),
+    path('personal/<int:doc_id>/', PersonalDocDetail.as_view(), name='personal_detail'),
 
 ]
